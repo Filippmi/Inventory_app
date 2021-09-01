44b814b6156dc5fe0cb663bd2ae45b4acce07b6a
Filipp Minayev <Filippmi@gmail.com>
2021-09-01 16:44:04 -0600

    new drink widget basic layout

---
+++ lib/widgets/new_drink.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewDrink extends StatefulWidget {
+
+  @override
+  _NewDrinkState createState() => _NewDrinkState();
+}
+
+class _NewDrinkState extends State<NewDrink> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
