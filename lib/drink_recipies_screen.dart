a53f5bf5e35f3dfaa41e5b4b12961c64efc97dec
Filipp Minayev <Filippmi@gmail.com>
2021-09-01 14:52:57 -0600

    added a drink recipies screen layout

---
+++ lib/drink_recipies_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DrinkRecipies extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drink Name'),
+      ),
+      body: Center(
+        child: Text('Recipe'),
+      ),
+    );
+  }
+}
