478c1e9d3ec11e1f1c042eec2e7a9dc60ecc3ae2
Filipp Minayev <Filippmi@gmail.com>
2021-09-02 07:01:51 -0600

    created submit function

--- lib/widgets/new_drink.dart
+++ lib/widgets/new_drink.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
 class NewDrink extends StatefulWidget {
+  final Function addDrink;
+
+  NewDrink(this.addDrink);
+
   @override
   _NewDrinkState createState() => _NewDrinkState();
 }
 
 class _NewDrinkState extends State<NewDrink> {
+  final _nameController = TextEditingController();
+
+  void _submitData() {
+    if (_nameController.text.isEmpty) {
+      return;
+    }
+    final enteredName = _nameController.text;
+
+    widget.addDrink(
+      enteredName,
+    );
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -16,7 +34,7 @@ class _NewDrinkState extends State<NewDrink> {
           children: [
             TextField(
               decoration: InputDecoration(labelText: 'Drink Name'),
-              controller: null,
+              controller: _nameController,
             ),
             RaisedButton(
               child: Text('Add Drink'),
