589f5688f0c336b765bc8aa22376e1bf12737d21
Filipp Minayev <Filippmi@gmail.com>
2021-09-09 17:37:07 -0600

    adding an ingredient variable to drink class

--- lib/models/drink.dart
+++ lib/models/drink.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class Drink {
   final String id;
   final String name;
+  final List<String> ingredients;
 
-  Drink({
+  const Drink({
     @required this.id,
     @required this.name,
+    @required this.ingredients,
   });
 }
