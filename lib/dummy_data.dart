35dea9c2abaa33c868d23f6c2936808e893aa28f
Filipp Minayev <Filippmi@gmail.com>
2021-09-09 16:40:27 -0600

    creating dummy data

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+import './models/drink.dart';
+
+const DUMMY_DRINKS = [
+  Drink(
+    id: 'd1',
+    name: 'Latte',
+    ingredients: [
+      '2 cups Milk',
+      '1 cup brewed espresso coffee',
+    ],
+  ),
+];
